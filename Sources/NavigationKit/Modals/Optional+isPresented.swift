42805debc35634478186930ef92d1ba78b5b3558
Andreas Link <mail@linkandreas.de>
2026-07-02 12:48:13 +0200

    docs: Improve documentation

--- Sources/NavigationKit/Modals/Optional+isPresented.swift
+++ Sources/NavigationKit/Modals/Optional+isPresented.swift
@@ -1,6 +1,10 @@
-import Foundation
+import SwiftUI
 
 extension Optional {
+    /// A helper property that maps an optional value to a boolean binding.
+    ///
+    /// - Returns: `true` if the optional is non-nil; `false` otherwise.
+    /// - Note: When setting to `false`, the optional is mutated to `nil`. Setting to `true` has no effect.
     var isPresented: Bool {
         get { self != nil }
         set {
